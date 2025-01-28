cb89d75df86aa678bf1c0c6d5194a170ab279df8
pavelux00x <pavelfilingeri.2@proton.me>
2025-01-28 12:19:30 +0100

    First and last commit

---
+++ describe.py
@@ -0,0 +1,25 @@
+from ollama import chat
+import base64
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+image_path = "id_fr.png"
+
+messages = [
+    {
+        'role': 'user',
+        'content': "AS OCR take the data and convert it to text as json format",
+        'images': [encode_image(image_path)]
+    },
+]
+
+stream = chat(
+    model='llava',
+    messages=messages,
+    stream=True,
+)
+
+for chunk in stream:
+    print(chunk['message']['content'], end='', flush=True)
